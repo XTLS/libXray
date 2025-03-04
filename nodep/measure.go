70f61ad2cb9692543bd9b446024f22d82f72ef63
yiguo <yiguo@example.com>
2025-03-04 23:02:35 +0800

    MeasureDelay supports no proxy

--- nodep/measure.go
+++ nodep/measure.go
@@ -1,6 +1,7 @@
 package nodep
 
 import (
+	"math"
 	"net/http"
 	"net/url"
 	"time"
@@ -14,14 +15,14 @@ const (
 // get the delay of some outbound.
 // timeout means how long the http request will be cancelled if no response, in units of seconds.
 // url means the website we use to test speed. "https://www.google.com" is a good choice for most cases.
-// proxy means the local http/socks5 proxy, like "socks5://[::1]:1080".
+// proxy means the local http/socks5 proxy, like "socks5://[::1]:1080". If proxy is empty, it means no proxy.
 func MeasureDelay(timeout int, url string, proxy string) (int64, error) {
 	httpTimeout := time.Second * time.Duration(timeout)
 	c, err := CoreHTTPClient(httpTimeout, proxy)
 	if err != nil {
 		return PingDelayError, err
 	}
-	delay, err := PingHTTPRequest(c, url)
+	delay, err := PingHTTPRequest(c, url, timeout)
 	if err != nil {
 		return delay, err
 	}
@@ -32,9 +33,12 @@ func MeasureDelay(timeout int, url string, proxy string) (int64, error) {
 func CoreHTTPClient(timeout time.Duration, proxy string) (*http.Client, error) {
 	tr := &http.Transport{
 		DisableKeepAlives: true,
-		Proxy: func(r *http.Request) (*url.URL, error) {
+	}
+
+	if len(proxy) > 0 {
+		tr.Proxy = func(r *http.Request) (*url.URL, error) {
 			return url.Parse(proxy)
-		},
+		}
 	}
 
 	c := &http.Client{
@@ -45,12 +49,18 @@ func CoreHTTPClient(timeout time.Duration, proxy string) (*http.Client, error) {
 	return c, nil
 }
 
-func PingHTTPRequest(c *http.Client, url string) (int64, error) {
+func PingHTTPRequest(c *http.Client, url string, timeout int) (int64, error) {
 	start := time.Now()
 	req, _ := http.NewRequest("HEAD", url, nil)
 	_, err := c.Do(req)
+	delay := time.Since(start).Milliseconds()
 	if err != nil {
-		return PingDelayTimeout, err
+		precision := delay - int64(timeout)*1000
+		if math.Abs(float64(precision)) < 50 {
+			return PingDelayTimeout, err
+		} else {
+			return PingDelayError, err
+		}
 	}
-	return time.Since(start).Milliseconds(), nil
+	return delay, nil
 }
