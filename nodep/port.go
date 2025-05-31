d44dc9ca582ea9b05d68d76079381b940eadae08
Tamim Hossain <tamimh.dev@gmail.com>
2025-06-01 04:50:21 +0600

    Fix looop lint issue
    Issue : `for loop can be modernized using range over int (modernize rangeint)`
    
    updated the implementation as I did in my hysteria wrapper [libHysteria2](https://github.com/CodeWithTamim/libHysteria2/blob/04ef7b784199ad16bc86da2d799b393fa886e4b3/nodep/port.go#L7)

--- nodep/port.go
+++ nodep/port.go
@@ -6,9 +6,10 @@ import (
 
 // https://github.com/phayes/freeport/blob/master/freeport.go
 // GetFreePort asks the kernel for free open ports that are ready to use.
+
 func GetFreePorts(count int) ([]int, error) {
 	var ports []int
-	for i := 0; i < count; i++ {
+	for range count {
 		addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
 		if err != nil {
 			return ports, err
@@ -18,8 +19,8 @@ func GetFreePorts(count int) ([]int, error) {
 		if err != nil {
 			return ports, err
 		}
-		defer l.Close()
 		ports = append(ports, l.Addr().(*net.TCPAddr).Port)
+		l.Close()
 	}
 	return ports, nil
 }
